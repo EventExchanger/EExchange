dd3491667992e23f203b22d44b55e68fd8fa7a4f
Ondriy <reactyii@mail.ru>
2021-07-20 09:34:49 +0300

    сборка

--- lib/esm/EExchange.d.ts
+++ lib/esm/EExchange.d.ts
@@ -1,36 +1,13 @@
-export interface Hash<T> {
-    [s: string]: T;
-}
 export declare type CB<T> = (event: iEvent<T>) => void;
 export interface iEvent<T> {
     name: string;
     initiator: object;
     data?: T;
 }
-export declare class EExchange {
-    static subscribeEvent<T>(groups: string[], cb: CB<T>): void;
-    static unsubscribeEvent<T>(groups: string[], cb: CB<T>): void;
-    static raiseEvent<T>(event: iEvent<T>): void;
+declare class EExchange {
+    subscribeEvent<T>(groups: string[], cb: CB<T>): void;
+    unsubscribeEvent<T>(groups: string[], cb: CB<T>): void;
+    raiseEvent<T>(event: iEvent<T>): void;
 }
-export declare class Console {
-    static log: {
-        (...data: any[]): void;
-        (message?: any, ...optionalParams: any[]): void;
-    };
-    static info: {
-        (...data: any[]): void;
-        (message?: any, ...optionalParams: any[]): void;
-    };
-    static error: {
-        (...data: any[]): void;
-        (message?: any, ...optionalParams: any[]): void;
-    };
-    static debug: {
-        (...data: any[]): void;
-        (message?: any, ...optionalParams: any[]): void;
-    };
-    static warn: {
-        (...data: any[]): void;
-        (message?: any, ...optionalParams: any[]): void;
-    };
-}
+declare const eexchange: EExchange;
+export default eexchange;
